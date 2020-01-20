0736b70b56980d8d6bde2df52981752fd19db73a
Wojciech Sobocinski <wojciech.sobocinski@globallogic.com>
2020-01-20 15:35:50 +0100

    Initial commit

---
+++ app/src/main/java/com/checkmyhand/Card.kt
@@ -0,0 +1,26 @@
+package com.checkmyhand
+
+enum class Figure(val figureValue: Int) {
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(11),
+    QUEEN(12),
+    KING(13),
+    ACE(14)
+}
+
+enum class Color(val color: Int) {
+    SPADES(1),
+    HEARTS(2),
+    DIAMONDS(3),
+    CLUBS(4)
+}
+
+data class Card(val figure: Figure, val color: Color)
